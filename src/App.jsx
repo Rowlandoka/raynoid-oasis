e84860ccd349b74f55ed55cbe0e9d4b59aaf19ac
Rowland Oka <rowlandoka@gmail.com>
2024-01-21 03:31:13 +0100

    setup path routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
-	return <div>Hello, World!</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='dashboard' element={<Dashboard />}></Route>
+				<Route path='bookings' element={<Bookings />}></Route>
+				<Route path='cabins' element={<Cabins />}></Route>
+				<Route path='users' element={<Users />}></Route>
+				<Route path='settings' element={<Settings />}></Route>
+				<Route path='account' element={<Account />}></Route>
+				<Route path='login' element={<Login />}></Route>
+				<Route path='*' element={<PageNotFound />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
