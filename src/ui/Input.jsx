5cf49d82b9aef553aadd27d6dc5579a9597b4ba4
Rowland Oka <rowlandoka@gmail.com>
2024-01-23 07:47:39 +0100

    create add new cabin functions

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Input = styled.input`
-	border: var(--color-gray-300);
-	background-color: var(--color-gray-0);
+	border: var(--color-grey-100);
+	background-color: var(--color-grey-0);
 	border-radius: var(--border-radius-sm);
 	box-shadow: var(--shadow-sm);
 	padding: 0.8rem 1.2rem;
