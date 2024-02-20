31ee7cffc519c0e226f3f16b93c023a80b77ee47
Rowland Oka <rowlandoka@gmail.com>
2024-02-20 20:19:18 +0100

    Implemented authentication and authorization feature

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AppLayout from './ui/AppLayout';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import Checkin from './pages/Checkin';
+import ProtectedRoute from './ui/ProtectedRoute';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -31,7 +32,13 @@ function App() {
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
-					<Route element={<AppLayout />}>
+					<Route
+						element={
+							<ProtectedRoute>
+								<AppLayout />
+							</ProtectedRoute>
+						}
+					>
 						<Route index element={<Navigate replace to='dashboard' />}></Route>
 						<Route path='dashboard' element={<Dashboard />}></Route>
 						<Route path='bookings' element={<Bookings />}></Route>
